4d62d5a9f8fdd0c43040a2bffb13ac504ad0d934
chandem <49794721+chandem@users.noreply.github.com>
2022-06-30 18:34:58 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: return 0
+ */
+int main(void)
+{
 putcher("and that piece of art is useful" - Dora Korpar, 2015-10-19 ");
+return (0);
+}
