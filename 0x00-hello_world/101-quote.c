0d7550dcaabdc07b317ca179cf069a6c7e680fa0
chandem <49794721+chandem@users.noreply.github.com>
2022-06-30 20:41:12 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\r\n", 62);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\r\n", 59);
 return (0);
 }
