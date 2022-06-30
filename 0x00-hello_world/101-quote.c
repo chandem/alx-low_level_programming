093ec864746fa929ff141f1435776a13fa3198f3
chandem <49794721+chandem@users.noreply.github.com>
2022-06-30 19:24:54 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  *main - Entry point
  *
  *Return: return 0
  */ 
-int write(int filedes, const char *buf, unsigned int nbyte);
-int main(int argc, char** argv)
+int main()
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 13);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 11);
 return (0);
 }
