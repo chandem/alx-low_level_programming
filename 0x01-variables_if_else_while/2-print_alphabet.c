64445230a30fc40e32400ebd1527142e6c9860a8
chandem <49794721+chandem@users.noreply.github.com>
2022-07-01 17:40:43 +0300

    Update 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+int n;
+for(n='a'; n<='z'; n++)
+{
+putchar(n);
+}
+return 0;
+}
