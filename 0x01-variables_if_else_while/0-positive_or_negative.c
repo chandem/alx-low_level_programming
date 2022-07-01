74dfd99399bdaabd295827152703486ac0f060f5
chandem <49794721+chandem@users.noreply.github.com>
2022-07-01 13:26:57 +0300

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,13 +13,13 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n>0){
-printf( n,"is positive\n");
+printf("is positive\n");
 }
 if(n<0){
-printf( n,"is negative\n");
+printf("is negative\n");
 }
 if(n==0){
-printf( n,"is zero\n");
+printf("is zero\n");
 }	
 	return (0);
 }
