1819790ac6bf260e87084f99086d4b598a088f10
chandem <49794721+chandem@users.noreply.github.com>
2022-07-02 00:22:26 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,10 +18,15 @@ for(z=y+1;z<10;z++)
 putchar(x + '0');
 putchar(y + '0');
 putchar(z + '0');
+if((x=8)&&(y==8)&&(z==9))
+{
+break;
+}
 putchar (',');
 putchar (' ');
 }
 }
-}   
+} 
+putchar ('\n');  
 return 0;
 }
