bfab567412620fb75dab252aedcd2ab3ffd89128
chandem <49794721+chandem@users.noreply.github.com>
2022-07-01 15:13:44 +0300

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ if(n%10>5){
 printf("%s%d%s%d%s" , "Last digit of ",n," is ",n%10," and is greater than 5\n");
 }
 if(n%10==0){
-printf("%s%d%s%d%s" , "Last digit of ",n," is ",n%10," and is zero\n"); 
+printf("%s%d%s%d%s" , "Last digit of ",n," is ",n%10," and is 0\n"); 
 }
 if(n%10<6 && n%10!=0){
 printf("%s%d%s%d%s" , "Last digit of ",n," is ",n%10," and is less than 6 and not 0\n"); 
