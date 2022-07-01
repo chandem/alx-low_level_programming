8187ad4010c7be8bf6a57941db8f82736aa90631
chandem <49794721+chandem@users.noreply.github.com>
2022-07-01 18:00:41 +0300

    Update 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+int n;
+for(n='a'; n<'e'; n++)
+{
+putchar(n)
+}
+for(n='f'; n<'q'; n++)
+{
+putchar(n)
+}
+for(n='r'; n<='z'; n++)
+{
+putchar(n)
+}
+return 0;
+}
