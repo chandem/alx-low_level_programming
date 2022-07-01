555867ccebdbf527f4f8bf114246c04b31696630
chandem <49794721+chandem@users.noreply.github.com>
2022-07-01 20:36:11 +0300

    Update 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: return 0
+ */
+int main(void)
+{
+int i=0;
+while(i<10){
+putchar(i + '0');     
+putchar("\,");
+putchar(" ");
+i++;
+ }
+putchar('\n');
+    return 0;
+}
