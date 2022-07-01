bc5a4c7ee97db641af3c4d9d0712bc0fad3c1616
chandem <49794721+chandem@users.noreply.github.com>
2022-07-01 16:51:36 +0300

    Update 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: return 0
+ */
+int main(void)
+{
+for(int  = 0; i<= 10; i++) 
+    {
+        
+        printf("%d" ,i);
+    }
+    return 0;
+}
