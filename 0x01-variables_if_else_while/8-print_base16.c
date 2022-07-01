208d36bc8e6d983c2da469f6478c2ba4c4781cee
chandem <49794721+chandem@users.noreply.github.com>
2022-07-01 20:21:14 +0300

    Update 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: return 0
+ */
+int main(void)
+{
+int i=0;
+while(i<10){
+        
+putchar(i + '0');
+i++;
+ }
+while(i<='e')
+{
+putchar (i + '0');
+}
+putchar ('\n');
+    return 0;
+}
