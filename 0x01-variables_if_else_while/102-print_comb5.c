198e9854941017829af8754a583e61020d5760ba
chandem <49794721+chandem@users.noreply.github.com>
2022-07-02 13:33:18 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,25 +7,24 @@
 int main()
 {
 int x;
+int y;
 for(x=0;x<100;x++)
 {
-int y;
 for(y=x+1;y<100;y++)
- {
-   putchar(x + '0');
-   putchar(x + '0');
+{
+putchar(x + '0');
+putchar(x + '0');
 
-   putchar(' ');
+putchar(' ');
 
-   putchar(x + '0');
-   putchar(y + '0');
+putchar(x + '0');
+putchar(y + '0');
+   
    
      
-   
-     
-   putchar(',');
-   putchar(' ');
- }
+putchar(',');
+ putchar(' ');
+}
 } 
 putchar('\n');  
 return 0;
