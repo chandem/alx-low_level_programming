451e6c671d84c46faee7327f49da3427cd5a93d1
chandem <49794721+chandem@users.noreply.github.com>
2022-07-05 20:12:36 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,1 +1,17 @@
+include"main.h"
+void jack_bauer(void){
+int j;
+for(j=0;j<=23;j++)
+{
+int i;
+for (i=0;i<=60;i++)
+{
+_putchar('0'+j%10);
+_putchar('0'+j/10);
+_putchar(':');
+_putchar('0'+i%10);
+_putchar('0'+i/10);
+}
+}
+}
 
