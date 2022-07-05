19d3db68df786638e8dda23ef2dc96fa693524ea
chandem <49794721+chandem@users.noreply.github.com>
2022-07-05 14:25:12 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,19 @@
-
+#include"main.h"
+/**
+ *main - Entry point
+ *
+ *Return: return 0
+ */
+int main (void)
+{
+print_alphabet_10x();
+putchar('\n');
+return 0;
+}
+void print_alphabet_10x(void){
+int i;
+for(i='a';i<='z';i++)
+{
+putchar(i);
+}
+}
