b09ca67d89270c7b0a79169d137b8492af1ec43f
chandem <49794721+chandem@users.noreply.github.com>
2022-07-05 22:53:10 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,13 +1,16 @@
 #include"main.h"
 void times_table(void)
 {
+int s;
 int j;
 for(j=0;j<=9;j++)
 {
 int i;
 for(i=0;i<=8;i++)
 {
-_putchar('0' + (i*j));
+s=i*j;
+_putchar('0' + s/10);
+_putchar('0' + s%10);
 }
 _putchar('\n');
 }
