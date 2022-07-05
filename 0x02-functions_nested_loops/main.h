5b31eae55d9af136e6faa87eb82a10a8390dd30a
chandem <49794721+chandem@users.noreply.github.com>
2022-07-05 15:26:31 +0300

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,11 @@
 int putchar(int);
-void print_alphabet(void);
+void print_alphabet(void){
+int i;
+for(i='a';i<='z';i++)
+{
+putchar(i);
+}
+}
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
