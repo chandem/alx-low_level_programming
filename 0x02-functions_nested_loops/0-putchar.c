0263904b3a4adec70e56e0b49fe1ddce3d15f2c2
chandem <49794721+chandem@users.noreply.github.com>
2022-07-05 13:50:26 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,15 @@
 #include"main.h"
+
 int main()
 {
 	putchar('-');
 	putchar('p');
-        putchar('-');                                                           
-        putchar('p');  	
+        putchar('u');                                                           
+        putchar('t'); 
+        putchar('c');
+        putchar('h');
+        putchar('a');
+        putchar('r');
+        putchar('\n');
+        return 0;
+}
