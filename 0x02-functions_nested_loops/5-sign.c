182401be2f841e36276d8c3d54001ac5550aa9ff
chandem <49794721+chandem@users.noreply.github.com>
2022-07-05 17:53:08 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,19 +4,22 @@
  *
  *Return: return 0
  */
-int main (void)
-{
 
-}
-
+int print_sign(int);
 int print_sign(int n){
 if(n>0)
+{
 putchar ('+');
 return 0;
+}
 else if(n==0)
+{
 putchar ('0');
 return 0;
-else if (n<0)
+}
+else
+{
 putchar ('-');
 return -1;
+}
 }
