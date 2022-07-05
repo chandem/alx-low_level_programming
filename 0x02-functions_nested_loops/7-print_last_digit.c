4230258f54626af88b6963c6f218ebe0cbc51c2b
chandem <49794721+chandem@users.noreply.github.com>
2022-07-05 23:25:22 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,13 +4,13 @@ int print_last_digit(int r)
 int d;
 if(r>=0)
 {
-d=r%10;
+d=(r%10);
 _putchar('0' + d);
 return (d);
 }
 if(r<0)
 {
-d=-1*(r%10);
+d=(-1*(r%10));
 _putchar('0' + d);
 return (d);
 }
