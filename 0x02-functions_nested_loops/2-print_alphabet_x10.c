691d1907d390341f0916f33fcfd02b835198f50f
chandem <49794721+chandem@users.noreply.github.com>
2022-07-05 16:54:27 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,21 +5,15 @@
  *prints lowercase 10x
  */
 void print_alphabet_x10(void);
-void print_alphabet_x10(void);
-void print_alphabet_x10(void);
-void print_alphabet_x10(void);
-void print_alphabet_x10(void);
-void print_alphabet_x10(void);
-void print_alphabet_x10(void);
-void print_alphabet_x10(void);
-void print_alphabet_x10(void);
-void print_alphabet_x10(void);
-
 void print_alphabet_x10(void){
+int j;
+for(j=0;j<=10;j++)
+{
 int i;
 for(i='a';i<='z';i++)
 {
 putchar(i);
 }
 putchar('\n');
+}
 }
