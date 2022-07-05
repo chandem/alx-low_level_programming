d923a50b31a999ddcf209553afe4e402ce50b577
chandem <49794721+chandem@users.noreply.github.com>
2022-07-05 22:42:34 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,11 +7,11 @@ for(j=0;j<=23;j++)
 int i;
 for (i=0;i<60;i++)
 {
-_putchar('0'+j/10);
-_putchar('0'+j%10);
+_putchar('0' + (j/10));
+_putchar('0' + (j%10));
 _putchar(':');
-_putchar('0'+i/10);
-_putchar('0'+i%10);
+_putchar('0' + (i/10));
+_putchar('0' + (i%10));
 }
 }
 }
