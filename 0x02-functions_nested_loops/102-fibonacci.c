14e3e02907c42d2c8afb76bd9d1252e3d1b35210
chandem <49794721+chandem@users.noreply.github.com>
2022-07-06 21:42:52 +0300

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -4,11 +4,15 @@ int main(void)
 int i,c;
 int a=0;
 int b=1;
-for (i = 1; i<50; i++)
+for (i = 1; i<=50; i++)
   {
       c=a+b;
       a=b;
       b=c;
+if(i==50)
+{
+break;
+}
     printf("%d", c);
     printf(", ");
   }
