789f2bef05bcb1cba4859b9d07b00e5befa9e059
chandem <49794721+chandem@users.noreply.github.com>
2022-07-06 11:44:52 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,13 +1,15 @@
 #include"main.h"
 void print_times_table(int n)
+int s;
+int j;
+int i;
 {
 if((n>=0) && (n<=15))
 {
-int s;
-int j;
+
 for(j=0;j<=n;j++)
 {
-int i;
+
 for(i=0;i<=n;i++)
 {
 s=i*j;
