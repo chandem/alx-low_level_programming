319b32ebedb11921629aaae17c3e39ea6c58726e
chandem <49794721+chandem@users.noreply.github.com>
2022-07-06 15:08:14 +0300

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,9 @@ int main(void)
 
         i = 0;
 
-        while (i < 10)      /** 
-                            * this line of code is causing infinite loop 
-        {                   *
-                putchar(i); */
+        while (i < 10)      /** this line of code is causing infinite loop */ 
+        {                   
+                putchar(i); 
         }                   
 
         printf("Infinite loop avoided! \\o/\n");
