a57867f92d99e7905dc66525213b07602d546f98
chandem <49794721+chandem@users.noreply.github.com>
2022-07-06 14:26:03 +0300

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -6,26 +6,4 @@ i=98;
 positive_or_negative(i);
 return 0;
 }
-void positive_or_negative(int n)
-{
 
-
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-
-if (n == 0)
-{
-printf("%i is zero\n", n);
-}
-
-else if (n < 0)
-{
-printf("%i is negative\n", n);
-}
-
-else
-{
-printf("%i is positive\n", n);
-}
-
-}
