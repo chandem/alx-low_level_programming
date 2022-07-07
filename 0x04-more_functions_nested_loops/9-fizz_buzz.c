bbc5bf22d0d92a4ca5a7d9545fa4098ab55ec792
chandem <49794721+chandem@users.noreply.github.com>
2022-07-07 13:50:12 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,17 +9,17 @@ int main (void)
 int i;
 for(i=1;i<=100;i++)
 {
-if(i%3==0)
+if(i%3==0 && i%5==0)
+{
+printf("%s","FizzBuzz");
+}
+else if(i%3==0)
 {
 printf("%s","Fizz");
 }
 else if(i%5==0)
 {
 printf("%s","Buzz");
-}
-else if(i%3==0 && i%5==0)
-{
-printf("%s","FizzBuzz");
 }
 else
 {
