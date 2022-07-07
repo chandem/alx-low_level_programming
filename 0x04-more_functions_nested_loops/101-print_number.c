3c9b3ab3f13fb4dc865ad9b6cf02570b32a9eeca
chandem <49794721+chandem@users.noreply.github.com>
2022-07-07 19:15:03 +0300

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,8 +1,10 @@
 #include"main.h"
 void print_number(int n)
 {
-if(n>=-2,147,483,647 && n<=2,147,483,647)
+if(n>=-2147483647 && n<=2147483647)
 {
+_putchar(n/1000000000 + '0');
+_putchar(n/100000000 + '0')
 _putchar(n/10000000 + '0');
 _putchar(n/1000000 + '0');
 _putchar(n/100000 + '0');
