a94c88d6dd87d84c4c2cfd09fa04277ebbed6668
chandem <49794721+chandem@users.noreply.github.com>
2022-07-07 10:11:01 +0300

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,22 @@
+#include"main.h"
+/**
+*print_most_numbers is function that prints number from 0 to 9
+*/
 void print_most_numbers(void)
 {
-
+int i;
+for(i=0;i<=9;i++)
+{
+if(i==2)
+{
+continue;
 }
+if(i==4)
+{
+continue;
+}
+_putchar(i + '0');
+}
+_putchar('\n');
+}
+
