a1ac8787a31ddecc552efc0e3d0a3feb522ece0b
chandem <49794721+chandem@users.noreply.github.com>
2022-07-07 18:25:59 +0300

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -2,18 +2,19 @@
 /**
  * main - Entry point.
  *
- * Return: Always 0.
+ * Return: Return 0.
  */
 int main(void)
 {
 int unsigned long r=612852475143LU;
 int unsigned long i;
-int unsigned long prime=0LU;
 for(i=2LU;i<r;i++)
-if(r%i==0)
+{
+while(r%i==0)
 {
 r=r/i;
 }
 printf("%llu",r);
 return 0;
+}
 }
