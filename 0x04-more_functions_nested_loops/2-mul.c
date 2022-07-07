fecac22dcbd7c34ce0b4211fba9cc2637c38b446
chandem <49794721+chandem@users.noreply.github.com>
2022-07-07 09:57:58 +0300

    Update 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include"main.h"
+/**
+*@a is input parameter
+*@b is input parameter
+*function returns the multiplication of two number
+*/
+int mul(int a, int b)
+{
+int mult=a*b;
+return mult;
+}
