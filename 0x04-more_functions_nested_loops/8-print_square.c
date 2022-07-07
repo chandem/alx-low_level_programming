25c539ff15f0687ee20c4c82cfe3a5f041d0df9b
chandem <49794721+chandem@users.noreply.github.com>
2022-07-07 11:29:55 +0300

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,3 +1,25 @@
-
-
-void print_square(int size);
+#include"main.h"
+/**
+*@n is input parameter
+*@print_square is function that prints diagonal line 
+*/
+void print_square(int size)
+{
+if(size<=0)
+{
+_putchar('\n');
+}
+else 
+{
+int j;
+for(j=0;j<size;j++)
+{
+int i;
+for(i=0;i<size;i++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
