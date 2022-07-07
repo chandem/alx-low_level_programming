2a349cfd3766d67ad7f5a99ecfe2d8558e4954de
chandem <49794721+chandem@users.noreply.github.com>
2022-07-07 16:59:30 +0300

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,16 +1,12 @@
 #include<stdio.h>
 int main(void)
 {
-int r=612852475143;
+unsigned long int r=612852475143;
 int i;
 int prime;
 for(i=1;i<r;i++)
 {
-if(r%i==0)
-{
-prime=(r/i);
-}
-
+prime=i;
 }
 printf("%d",prime);
 return 0;
