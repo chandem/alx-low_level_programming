638fe24067867ffdd1f1eacb93ba97fa76108087
chandem <49794721+chandem@users.noreply.github.com>
2022-07-07 10:26:45 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,21 @@
+include"main.h"
+/**
+*@more_numbers function that prints number from 0 to 9
+*/
+void more_numbers(void)
+{
+int j;
+for(j=0;j<=9;j++)
+{
+int i;
+for(i=0;i<=14;i++)
+{
+_putchar(i/10 + '0');
+_putchar(i%10 + '0');
+}
+_putchar('\n');
+}
+}
 
 
 
-void more_numbers(void);
