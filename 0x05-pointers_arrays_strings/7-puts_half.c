d96545c649fa504e7ac0492cc031e8f45f4d04d1
chandem <49794721+chandem@users.noreply.github.com>
2022-07-11 17:13:58 +0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,9 +9,21 @@ char *t=str;
 int i;
 int j;
 for(i=0;t[i]!='\0';i++);
+if(i%2==0)
+{
 for(j=i/2;j<i;j++)
 {
 _putchar(t[j]);
+}
+}
+else
+{
+i=i-1;
+for(j=i/2;j<i;j++)
+{
+_putchar(t[j]);
+}
+
 }
 _putchar('\n');
 }
