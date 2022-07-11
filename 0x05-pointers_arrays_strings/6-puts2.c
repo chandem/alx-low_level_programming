91591acc2ce1896e50ed1d2e0ca7364509ad4fcc
chandem <49794721+chandem@users.noreply.github.com>
2022-07-11 16:06:05 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,17 @@
-
+#include"main.h"
+/**
+*puts2 -prints the string of given char. on stdout.
+*@str: is input parameter.
+*/
 void puts2(char *str)
 {
+char *t=str;
+int i;
+for(i=0;t[i]!='\0';i++);
+int j;
+for(j=0;j<i;j++)
+{
+_putchar(t[j]);
+}
+_putchar('\n');
 }
