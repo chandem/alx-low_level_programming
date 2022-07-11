749f9efa1698296496adfd1f6d9221642fd5366c
chandem <49794721+chandem@users.noreply.github.com>
2022-07-11 12:54:45 +0300

    Update 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+*_strlen - returns the length of characters.
+*s: is input parameter
+*/
+int _strlen(char *s)
+{
+    char s[] = *s;
+    int i;
+    for (i = 0; s[i] != '\0'; ++i);
+return i;
+}
