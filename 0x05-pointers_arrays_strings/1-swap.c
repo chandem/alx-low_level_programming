d01c25e731306726e9ffcc7117f35b56dfa64144
chandem <49794721+chandem@users.noreply.github.com>
2022-07-11 12:14:52 +0300

    Update 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+*swap_int -swaps the value of two integers.
+*@a: input parameter
+*@b: input parameter
+*/
+void swap_int(int *a, int *b)
+{
+int temp;
+temp=*b;
+*b=*a;
+a*=temp;
+
+}
