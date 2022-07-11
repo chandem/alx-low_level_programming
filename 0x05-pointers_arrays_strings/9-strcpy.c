63f41fb509162e0885974af4e3c79d1aee3f9dee
chandem <49794721+chandem@users.noreply.github.com>
2022-07-11 21:23:53 +0300

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,12 @@
-
+#include"main.h"
+#include<stdio.h>
+/**
+*print_array -prints the array  of given number
+*@dest: is input parameter.
+*@src: is input parameter.
+*Return: char
+*/
 char *_strcpy(char *dest, char *src)
-{
 
+memcpy(dest, src, sizeof(src));
 }
