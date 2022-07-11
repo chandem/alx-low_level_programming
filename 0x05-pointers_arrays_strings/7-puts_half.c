629b1684b542fd113fbc9d5e143d35921e24b8a4
chandem <49794721+chandem@users.noreply.github.com>
2022-07-11 16:23:50 +0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,4 +1,18 @@
+#include"main.h"
+/**
+*puts_half -prints the string of given char. on stdout.
+*@str: is input parameter.
+*/
 void puts_half(char *str)
 {
-
+char *t=str;
+int i;
+for(i=0;t[i]!='\0';i++);
+i=i-1;
+i=i/2;
+while(i>=0)
+{
+_putchar(t[i]);
+}
+_putchar('\n');
 }
