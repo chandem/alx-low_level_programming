90b1ffa0309492cd2ba4c5fe64bf7b09b4f12eac
chandem <49794721+chandem@users.noreply.github.com>
2022-07-13 23:45:00 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,7 +2,17 @@
 /**
 *cap_string -The function change uppercase letters to lowercase.
 *@s: it is input parameter.
+*Return: (char*)s
 */ 
 char *cap_string(char *s)
 {
+int i;
+for (i = 0; s[i]!='\0'; i++) 
+{
+      if(s[i] >= 'a' && s[i] <= 'z')
+ {
+         s[i] = s[i] - 32;
+      }
+   }
+return (char*)s;
 }
