476dada2c11afdedf00f6fc86f5cf29d14b76afc
chandem <49794721+chandem@users.noreply.github.com>
2022-07-13 23:09:19 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,9 +6,12 @@
 char *string_toupper(char *s)
 {
 int i;
-for (i = 0; s[i]!='\0'; i++) {
-      if(s[i] >= 'a' && s[i] <= 'z') {
+for (i = 0; s[i]!='\0'; i++) 
+{
+      if(s[i] >= 'a' && s[i] <= 'z')
+ {
          s[i] = s[i] - 32;
       }
    }
+return (char*)s;
 }
