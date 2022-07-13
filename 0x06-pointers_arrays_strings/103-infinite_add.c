087e2d843a076b0fd2a4e88e8f3eab3bae416512
chandem <49794721+chandem@users.noreply.github.com>
2022-07-13 14:59:28 +0300

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,4 +1,12 @@
-
+#include"main.h"
+/**
+*infinite_add -The function add two numbers.
+*@n1: it is input parameter.
+*@n1: it is input parameter.
+*@r: it is input parameter.
+*@size_r: it is input parameter.
+*Return: char.
+*/ 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 
