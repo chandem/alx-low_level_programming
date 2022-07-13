0eeca179e8503fb26d29aa6c3bef497d617157b5
chandem <49794721+chandem@users.noreply.github.com>
2022-07-13 20:03:07 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,17 +8,15 @@
 */ 
 char *_strncat(char *dest, char *src, int n)
 {
-char s1[n]=dest;
-char s2[]=src;
 int i;
 int j;
-for (i = 0; s1[i]!='\0'; i++);
+for (i = 0; src[i]!='\0'; i++);
 i=i-1;
-  for (j = 0; s2[j]!='\0'; j++)
+  for (j = 0;j<n && dest[j]!='\0'; j++)
   {
 i++;
      s1[i] = s2[j];
   }
-s1[i+1]='\0';
-return (char)s1;
+dest[i+1]='\0';
+return (char*)dest;
 }
