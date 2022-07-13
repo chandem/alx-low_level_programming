b5dc5e37c806eb7bf731dfb5311021aa6aa31f69
chandem <49794721+chandem@users.noreply.github.com>
2022-07-13 21:05:33 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,5 +7,33 @@
 */ 
 int _strcmp(char *s1, char *s2)
 {
+int i;
+int j;
+int z;
+int a=0;
+for(j=0;s1[j]!='\0';j++);
+for(i=0;s2[i]!='\0';i++);
+if(j==i)
+{
+for(z=0;s1[z]!='\0';z++)
+{
+if(s1[z]==s2[z])
+a++;
+}
+if(a==j)
+{
+return 0;
+}
+else
+{
+return s1;
+return s2;
+}
+}
+else
+{
+return s1;
+return s2;
+}
 
 }
