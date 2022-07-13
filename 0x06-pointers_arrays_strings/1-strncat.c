8b9b462fa401ed69b4ce93dd8311d8e69bd979e0
chandem <49794721+chandem@users.noreply.github.com>
2022-07-13 14:25:23 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,11 @@
-
+#include"main.h"
+/**
+*_strcat -The function concatenate string s.
+*@dest: it is destination parameter where the string appended
+*@src: it is  source parameter where the string copied.
+*@n: it is input parameter.
+*Return: char(dest).
+*/ 
 char *_strncat(char *dest, char *src, int n)
 {
 
