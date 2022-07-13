a322e8146d0316db97a4c6c11995e8d885883a75
chandem <49794721+chandem@users.noreply.github.com>
2022-07-13 14:20:37 +0300

    Update 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+#include"main.h"
+/**
+*_strcat -The function concatenate string s.
+*@dest: it is destination parameter where the string appended
+*@src: it is  source parameter where the string copied.
+*Return: char(dest).
+*/ 
+char *_strcat(char *dest, char *src)
+{
+
+}
