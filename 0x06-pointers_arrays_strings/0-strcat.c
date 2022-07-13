1b20864a864eea56d34d5722eefbf071a20ca081
chandem <49794721+chandem@users.noreply.github.com>
2022-07-13 18:34:22 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,11 +10,13 @@ char *_strcat(char *dest, char *src)
 int i;
 int j;
 for (i = 0; dest[i]!='\0'; i++);
-  i=i-1;
+i=i-1;
   for (j = 0; src[j]!='\0'; j++)
   {
-     dest[i++] = src[j];
+i++;
+     dest[i] = src[j];
   }
+dest[i+1]='\n';
 return dest;
  
 }
