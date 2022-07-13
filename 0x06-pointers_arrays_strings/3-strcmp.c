3de1f66db79e4faad332e8645e41bc4043b63258
chandem <49794721+chandem@users.noreply.github.com>
2022-07-13 14:34:27 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,10 @@
-
+#include"main.h"
+/**
+*_strncmp -The function concatenate string s.
+*@s1: it is input parameter 
+*@s2: it is input parameter 
+*Return: int.
+*/ 
 int _strcmp(char *s1, char *s2)
 {
 
