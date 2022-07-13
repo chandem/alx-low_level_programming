41142afbd5fa01ff6b80c733e1c3894bbb3fb23a
chandem <49794721+chandem@users.noreply.github.com>
2022-07-13 14:44:33 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,6 +1,9 @@
-
-
-char *string_toupper(char *)
+#include"main.h"
+/**
+*string_toupper -The function change uppercase letters to lowercase.
+*@s: it is input parameter.
+*/ 
+char *string_toupper(char *s)
 {
 
 }
