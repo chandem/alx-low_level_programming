89beda28f56a65bb8b49ceb1dc731e80c4463f96
chandem <49794721+chandem@users.noreply.github.com>
2022-07-13 14:39:51 +0300

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,4 +1,9 @@
-
+#include"main.h"
+/**
+*reverse_array -The function reverse the item of given array.
+*@a: it is input parameter.
+*@n: it is input parameter.
+*/ 
 void reverse_array(int *a, int n)
 {
 
