0aabc1725971da1b4f444fa4f3e7da943f6daa32
chandem <49794721+chandem@users.noreply.github.com>
2022-07-14 21:29:09 +0300

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,9 +2,13 @@
 /**
 *print_buffer -The function prints buffer.
 *@b: it is input parameter.
-*siza: it is input parameter.
+*size: it is input parameter.
 */ 
 void print_buffer(char *b, int size)
 {
-
+int i;
+for(i=0;i<size;i++)
+{
+b[i];
+}
 }
