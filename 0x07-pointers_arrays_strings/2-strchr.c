7aee5494efe94aba8eb83a9cb32883d014849d8f
chandem <49794721+chandem@users.noreply.github.com>
2022-07-18 23:39:38 +0300

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,18 +8,11 @@
 char *_strchr(char *s, char c)
 {
 int i;
-int j;
 for(i=0;s[i]!='\0';i++);
-i=i-1;
-for(j=0;j<i;j++)
 {
-if(s[j]==c)
+if(s[i]==c)
 {
 return (char*)s;
-}
-else
-{
-return NULL;
 }
 }
 }
