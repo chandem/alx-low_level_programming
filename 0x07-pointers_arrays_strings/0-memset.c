d0715e75f7ce1ed6d7b52f9a8c50ab21673a23eb
chandem <49794721+chandem@users.noreply.github.com>
2022-07-18 21:02:50 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,17 @@
 #include"main.h"
+/**
+*_memset -The functions that set memory location.
+*@b: input parameter.
+*@s: input parameter.
+*@n: input parameter.
+*Return: char(s).
+*/
 char *_memset(char *s, char b, unsigned int n)
 {
-
+int i;
+for(i=0;i<n;i++)
+{
+s[i]=b;
+}
+return char*(s);
 }
