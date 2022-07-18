c482d6c0477c6e1fae7e258005b909fe0af26bb7
chandem <49794721+chandem@users.noreply.github.com>
2022-07-18 21:13:40 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,14 @@
 #include"main.h"
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-
+int i;
+int j;
+for(i=0;src[i]!='\0';i++);
+i=i-1;
+for(j=0;j<i;j++)
+{
+dest[j]=src[j];
+}
+return (char*)dest;
 }
