5c916f883517ea063562df1d4844a065a73cd1a4
chandem <49794721+chandem@users.noreply.github.com>
2022-07-19 10:43:53 +0300

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,12 +11,14 @@ unsigned int i;
 unsigned int j;
 unsigned int z=0;
 for(i=0;accept[i]!='\0';i++);
-for(j=0;j<i;j++)
+j=0;
+while(j<i)
 {
 if(accept[j]==s[j])
 {
 z++;
 }
+j++;
 }
 return (z);
 }
