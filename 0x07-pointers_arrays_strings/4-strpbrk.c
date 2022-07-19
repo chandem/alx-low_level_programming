36a6460888c2ce1d75d8668fa802171f1283cf97
chandem <49794721+chandem@users.noreply.github.com>
2022-07-19 10:25:48 +0300

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,1 +1,29 @@
 #include"main.h"
+/**
+*_strpbrk -function that return the first prefix string 
+*@s: input parameter.
+*@accept: input parameter.
+*Return: (char*)s.
+*/
+char *_strpbrk(char *s, char *accept)
+{
+unsigned int i;
+unsigned int j;
+unsigned int z=0;
+for(i=0;accept[i]!='\0';i++);
+for(j=0;j<i;j++)
+{
+if(accept[j]==s[j])
+{
+z++;
+}
+}
+if(z==j) 
+{
+return s;
+}
+else
+}
+return 0;
+}
+}
