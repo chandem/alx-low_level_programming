30949d6de6038e40f28b204e4ce6ee0823c00ed1
chandem <49794721+chandem@users.noreply.github.com>
2022-07-19 14:09:55 +0300

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,18 +8,16 @@
 char *_strpbrk(char *s, char *accept)
 {
 int i;
-int j;
-int z;
 int k;
 i=0;
-while(accept [i]!='\0')
+while(accept[i]!='\0')
 {
 k=0;
 while(s[k]!='\0')
 {
 if(accept[i]==s[k])
 {
-return accept[i];
+return s[k];
 }
 k++;
 }
