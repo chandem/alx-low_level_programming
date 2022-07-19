a3c190fcfd0a97b81a9f0eeac53402c6d469df01
chandem <49794721+chandem@users.noreply.github.com>
2022-07-19 10:01:51 +0300

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,22 @@
 #include"main.h"
+/**
+*_strpn -function that return the first prefix string 
+*@s: input parameter.
+*@accept: input parameter.
+*Return: unsigned int.
+*/
 unsigned int _strspn(char *s, char *accept)
 {
-
+unsigned int i;
+unsigned int j;
+unsigned int z=0;
+for(i=0;accept[i]!='\0';i++);
+for(j=0;j<i;j++)
+{
+if(accept[j]==s[j])
+{
+z++;
+}
+}
+return (z+1);
 }
