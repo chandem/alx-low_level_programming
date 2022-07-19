08eb176faa74bc6c93a5b96b5bb0a0432cc912ad
chandem <49794721+chandem@users.noreply.github.com>
2022-07-19 21:34:54 +0300

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,4 +1,15 @@
 #include"main.h"
+/**
+*print_chessboard -function that prints chessboard.
+*@a: input parameter.
+*/
 void print_chessboard(char (*a)[8])
 {
+int i;
+int j;
+for(j=0;a[j]!='\0';j++)
+for(i=0;i<8;i++)
+{
+return a[j][i];
+}
 }
