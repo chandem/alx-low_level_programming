fb3b3c3232c99829be4239d822127f787a439736
chandem <49794721+chandem@users.noreply.github.com>
2022-07-19 23:48:40 +0300

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,15 +8,15 @@ void print_chessboard(char (*a)[8])
 int i;
 int j;
 j=0;
-while (a[j]!='\0')
+while (j<8)
 {
 i=0;
 while(i<8)
 {
-printf("%c",a[j][i]);
+_putchar(a[j][i]);
 i++;
 }
-printf("\n");
+_putchar('\n');
 j++;
 }
 }
