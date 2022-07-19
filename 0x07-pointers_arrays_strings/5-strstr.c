59a5da4dd8722cad8a3ccbc01c5a634e160f72dc
chandem <49794721+chandem@users.noreply.github.com>
2022-07-19 15:54:08 +0300

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,6 +1,30 @@
 #include"main.h"
-#include<stdio.h>
 char *_strstr(char *haystack, char *needle)
 {
-
+int i;
+int k;
+int z;
+int j;
+i=0;
+j=0;
+for(j=0;needle[j]!='\0';j++);
+while(haystack[i]!='\0')
+{
+z=0;
+k=0;
+while(needle[k]!='\0')
+{
+if(needle[k]==haystack[i])
+{
+z++;
+}
+k++;
+}
+if(z==j-1)
+{
+return needle;
+}
+i++
+}
+}
 }
