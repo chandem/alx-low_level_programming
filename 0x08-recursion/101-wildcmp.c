249502562ab599a6c2674c6d6ae70774746f449c
chandem <49794721+chandem@users.noreply.github.com>
2022-07-20 23:13:42 +0300

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,12 +7,19 @@
 */
 int wildcmp(char *s1, char *s2)
 {
+if(*s1!='\0' && *s2!='\0')
+{
 if(wildcmp((s1+1),s2)==wildcmp(s1,(s2+1)))
 {
 return 1;
 }
 else
 { 
+return 0;
+}
+}
+else
+{
 return 0;
 }
 }
