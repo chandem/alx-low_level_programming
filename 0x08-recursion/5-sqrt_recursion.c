662b1759e215e4fbd8df7cb2d13ac4dd51ca5b83
chandem <49794721+chandem@users.noreply.github.com>
2022-07-20 20:01:58 +0300

    Update 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+*_sqrt_recursion -function that print square of natural number.
+*@n: input parameter.
+*Return: int
+*/
+int _sqrt_recursion(int n)
+{
+if( n<0)
+{
+return -1;
+}
+return n*n;
+}
