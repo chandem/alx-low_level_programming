cce225a8807ee383ad9f33b61dbfd7300943b271
chandem <49794721+chandem@users.noreply.github.com>
2022-07-20 21:47:45 +0300

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,7 +3,7 @@
 *_pow_recursion -function that print power of two numbers.
 *@x: input parameter.
 *@x: input parameter.
-Return: int 
+*Return: int 
 */
 int _pow_recursion(int x, int y)
 {
@@ -13,8 +13,7 @@ return -1;
 }
 if(y>1)
 {
-
-return x*_pow_recursion(x,(y-1));
+return (x*(_pow_recursion(x,(y-1))));
 }
 if(y==1)
 {
