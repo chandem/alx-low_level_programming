9d6651bdfebc5db7277019a63d8daa510e61e3e9
chandem <49794721+chandem@users.noreply.github.com>
2022-07-20 22:41:20 +0300

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,1 +1,17 @@
-
+#include"main.h"
+/**
+*is_palindrome -function that check whether the string is palindrome or not.
+*@s: input parameter.
+*Return: int 
+*/
+int is_palindrome(char *s)
+{
+if(is_palindrome(s+1)==is_palindrome(s-1))
+{
+return 1;
+}
+else
+{
+return 0;
+}
+}
