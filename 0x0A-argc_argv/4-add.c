8ed64b4a11455948a4f9e121207ce973ab3b7925
chandem <49794721+chandem@users.noreply.github.com>
2022-07-22 16:06:23 +0300

    Update 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/** 
+*main -Entry point.
+*@argc: input parameter.
+*@argv: input parameter.
+*Return: 0
+*/
+int main (int argc ,char *argv[])
+{
+if(argc<3)
+{
+printf("%s\n","error");
+return 1;
+}
+(void)argc;
+else
+{
+int r;
+r= (int)argv[1]+(int)argv[2];
+printf("%d\n",r);
+
+return
