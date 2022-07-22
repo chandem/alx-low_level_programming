fa0c864894600b24c0f5face58312fb5ec95a301
chandem <49794721+chandem@users.noreply.github.com>
2022-07-22 15:17:19 +0300

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,9 +5,9 @@
 *@argv: input parameter.
 *Return: 0
 */
-int main (int argc ,char cons *argv[])
+int main (int argc ,char *argv[])
 {
-void(argc);
+(void)argc;
 printf("%s\n",argv[0]);
 return 0;
 }
