b8ba918efdad8329ae90eca291d4fb72b50c5485
chandem <49794721+chandem@users.noreply.github.com>
2022-07-26 15:37:38 +0300

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,1 +1,11 @@
+#include"main.h"
+/**
+* create_array -function that create memory with given char.
+*@size: input parameter
+*@c: input parameter
+*Return: char
+*/
+char *create_array(unsigned int size, char c)
+{
 
+}
