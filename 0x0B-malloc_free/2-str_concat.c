3f30c144ed4fcb6707219e3727a66fe389a66509
chandem <49794721+chandem@users.noreply.github.com>
2022-07-26 22:25:01 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,9 +1,16 @@
 #include"main.h"
 /**
-*
-*
-*
+*str_concat -function that concatenate two strings.
+*@s1: input parameter.
+*@s2: input parameter.
+*Return: char 
 */
 char *str_concat(char *s1, char *s2)
 {
+char *f;
+unsigned i;
+unsigned j;
+for(i=0;*s1!='\0';i++);
+for(j=0;*s2!='\0';j++);
+f=malloc(i+j);
 }
