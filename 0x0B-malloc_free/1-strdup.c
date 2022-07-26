6c0c427e20844da16aa464c632448e1af2da6d4c
chandem <49794721+chandem@users.noreply.github.com>
2022-07-26 21:43:06 +0300

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,5 +1,21 @@
 #include"main.h"
+#include <stddef.h>
+/**
+*_strdup -function that returns pointer.
+*@str: input parameter.
+*/
 char *_strdup(char *str)
 {
+if(*str=='\0')
+{
+return NULL;
+}
+else if(*str!='\0')
+{
+return str;
+}
+else
+{
 
+}
 }
