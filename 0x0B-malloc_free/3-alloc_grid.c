9296fa017a2c94d7d05b0b143dcd44c0381f53a1
chandem <49794721+chandem@users.noreply.github.com>
2022-07-26 23:44:09 +0300

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,9 +1,29 @@
 #include"main.h"
 /**
-*
-*
-*
+*alloc_grid -function that returns two dimensional arrays.
+*@width: input parameter
+*@height: input parameter.
+*Return: int
 */
 int **alloc_grid(int width, int height)
 {
+int **f;
+int i;
+int j;
+f= malloc(sizeof(int)*(width*height));
+if(width<=0 || height<=0)
+{
+return 0;
+}
+else
+{
+for(i=0;i<height;i++)
+{
+for(j=0;j<width;j++)
+{
+f[i][j]=0;
+}
+}
+return f;
+}
 }
