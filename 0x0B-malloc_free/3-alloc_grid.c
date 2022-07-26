e398cc3dbe6465fddc4a4f1a5b5010c7559d3f32
chandem <49794721+chandem@users.noreply.github.com>
2022-07-27 01:04:46 +0300

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,7 +12,7 @@ int **alloc_grid(int width, int height)
 int **f;
 int i;
 int j;
-f=malloc(sizeof(int)*(width*height));
+f=(unsigned int)malloc(sizeof(int)*(width*height));
 if(width<=0 || height<=0)
 {
 return 0;
