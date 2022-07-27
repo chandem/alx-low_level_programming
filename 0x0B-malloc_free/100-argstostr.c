eb2e87b1df766e3eb881599ed215702c3834dce9
chandem <49794721+chandem@users.noreply.github.com>
2022-07-27 21:39:53 +0300

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,10 +1,19 @@
 #include"main.h"
+#include<stdlib.h>
 /**
-*
-*
-*
+*argstostr -function that concatenate given s.
+*@av: input parameter.
+*@ac: input parameter.
+*Return:char
 */
 char *argstostr(int ac, char **av)
+{ 
+char *d;
+int i;
+while(ac<=0)
 {
-
+for(i=0;av[ac][i]!='\0';i++);
+ac--;
+}
+d=malloc(sizeof(char)*(i+1));
 }
