803531aa443992b7afac07d98b8ea69d6387ef1b
chandem <49794721+chandem@users.noreply.github.com>
2022-07-27 19:43:14 +0300

    Update 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,10 +1,12 @@
 #include"main.h"
+#include<stdlib.h>
 /**
-*
-*
-*
+*free_grid -function that free allocated memory.
+*@grid: input parameter.
+*@height: input parameter.
 */
 void free_grid(int **grid, int height)
 {
-
+grid=malloc(sizeof(int*)*(height));
+free(grid);
 }
