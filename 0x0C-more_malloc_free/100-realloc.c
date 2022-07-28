0f8b38f4b4dc1ded95fbd191ae4ce9c9fdbaaae7
chandem <49794721+chandem@users.noreply.github.com>
2022-07-28 16:48:46 +0300

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,1 +1,11 @@
+#include"main.h"
+/**
+*_realloc -function that create array.
+*@ptr: input parameter.
+*@old_size: input parameter.
+*@new_size: input parameter.
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
 
+}
