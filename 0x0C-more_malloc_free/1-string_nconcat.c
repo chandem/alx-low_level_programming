e77c76329122a5aba50dbc705cbef88e8f03b6a7
chandem <49794721+chandem@users.noreply.github.com>
2022-07-28 16:22:40 +0300

    Update 1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+*sting_nconcat -function that concatenate two strings.
+*@s1: input parameter.
+*@s2: input parameter.
+*@n: input parameter.
+*Return: char.
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+}
