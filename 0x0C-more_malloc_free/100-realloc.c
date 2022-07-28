c758eee4360cc6b3b91d921a9c22a7cafc83b855
chandem <49794721+chandem@users.noreply.github.com>
2022-07-28 16:29:34 +0300

    Update 100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,9 @@
+#include"main.h"
+/**
+*_calloc -function that allocates memory for array.
+*@nmemb: input parameter.
+*@size: input parameter.
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+}
