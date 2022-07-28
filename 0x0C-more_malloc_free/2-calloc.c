3fa25dd451d38a97401352e38d93883062cfd2af
chandem <49794721+chandem@users.noreply.github.com>
2022-07-29 00:12:09 +0300

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,10 +9,22 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 unsigned int i;
 char **d;
+if(nmemb==0 ||size==0)
+{
+return NULL;
+}
 d=malloc(sizeof(char *)*nmemb);
+if(d==NULL)
+{
+return NULL;
+}
 for(i=0;i<nmemb;i++)
 {
 d[i]=malloc(sizeof(char)*size);
+if(d==NULL)
+{
+return NULL;
+}
 }
 return d;
 }
