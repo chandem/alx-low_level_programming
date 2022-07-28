6b30ad34dfbd4a6db13a52798c744b4a9bee6c25
chandem <49794721+chandem@users.noreply.github.com>
2022-07-28 16:36:34 +0300

    Update 3-array_range.c

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,11 @@
+#include"main.h"
+/**
+*array_range -function that create array of integers.
+*@min: input parameter.
+*@max: input parameter.
+*Return: int
+*/ 
+int *array_range(int min, int max)
+{
+
+}
