1884452ed89a68e7661824230b1de3f4d36b2dbe
chandem <49794721+chandem@users.noreply.github.com>
2022-07-28 22:27:15 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,5 +8,45 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-
+char *d;
+unsigned int i;
+unsigned int j;
+int k;
+int z;
+for(i=0;s1[i]!='\0';i++);
+for(j=0;s2[j]!='\0';j++);
+if(n<j)
+{
+d=malloc(sizeof(char)*(i+j+1));
+if(d==NULL)
+{
+return NULL;
+}
+for(k=0;k<i;k++)
+{
+d[k]=s1[k];
+}
+for(z=0;z<=j;z++)
+{
+d[k+z]=s1[z];
+}
+return d;
+}
+else
+{
+d=malloc(sizeof(char)*(i+n));
+if(d==NULL)
+{
+return NULL;
+}
+for(k=0;k<i;k++)
+{
+d[k]=s1[k];
+}
+for(z=0;z<n;z++)
+{
+d[k+z]=s1[z];
+}
+return d;
+}
 }
