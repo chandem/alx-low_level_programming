cdbe684c14f4044244509ba5ca52b972012eab78
chandem <49794721+chandem@users.noreply.github.com>
2022-08-01 22:06:10 +0300

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,23 @@
+#include<stdlib.h>
 #include"dog.h"
+/**
+*print_dog -print struct dog.
+*@d: input parameter.
+*/
 void print_dog(struct dog *d)
 {
-
+if(d->name==NULL && d->age==NULL && d->owner==NULL)
+{
+printf("%s","nil");
+}
+else if(d->name==NULL)
+{
+printf("Name:%s","nil");
+}
+else
+{
+printf("Name:%s",d->name);
+printf("Age:%f",d->age);
+printf("Owner:%s",d->owner);
+}
 }
