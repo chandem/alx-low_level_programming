cca54b8f8904edc524951e39e673f6e4084429d0
chandem <49794721+chandem@users.noreply.github.com>
2022-08-01 21:30:23 +0300

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,11 @@
 #include"dog.h"
+/**
+*init_dog -is function that intialize struct dog.
+*@d: input parameter.
+*@name: input parameter
+*@age: input parameter.
+*@owner: input parameter.
+*/
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 if (d)
