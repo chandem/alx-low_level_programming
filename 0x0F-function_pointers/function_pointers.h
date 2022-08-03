fc1a8aceceb6027c6f1722a164d18e9e3458f062
chandem <49794721+chandem@users.noreply.github.com>
2022-08-03 22:36:06 +0300

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
-#define MAIN_H
-#define MAIN_H
+#ifndef _FUNCTION_POINTERS_
+#define _FUNCTION_POINTERS_
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 
-#endef
+#endif
