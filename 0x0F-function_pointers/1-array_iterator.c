29c6503795bdf218a560a82c7331e681424d0068
chandem <49794721+chandem@users.noreply.github.com>
2022-08-03 19:53:22 +0300

    Update 1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,11 @@
+#include"function_pointers.h"
+/**
+*array_iterator -function that print array
+*@array: input parameter.
+*@size: input parameter.
+*@action: input parameter.
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+}
