ab604e9e0b0fadf0e4ee4ede06e91d26bf15eea5
chandem <49794721+chandem@users.noreply.github.com>
2022-08-03 17:18:15 +0300

    Update 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include"function_pointer"
+/**
+*print_name -function that print name
+*@name: input parameter.
+*@f: input parameter.
+*/
+void print_name(char *name, void (*f)(char *))
+{
+return name;
+}
