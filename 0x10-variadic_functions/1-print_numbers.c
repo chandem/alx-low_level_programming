f480ca5dc2a8598dc2e31f2d473493bd37e948e8
chandem <49794721+chandem@users.noreply.github.com>
2022-08-04 21:47:55 +0300

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,4 +1,5 @@
 #include<stdarg.h>
+#include<stdio.h>
 #include"variadic_function.h"
 /**
 *print_numbers -function that print numbers.
@@ -7,5 +8,16 @@
 */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-
+    unsigned int i;
+    va_list ptr;
+    va_start(ptr, separator, n);
+    
+    for (i = 0; i < n; i++)
+{
+  va_arg(ptr,char*, unsigned int);
+printf("%lu",n);
+printf("%s", separator);
+}
+    va_end(ptr);
+ 
 }
