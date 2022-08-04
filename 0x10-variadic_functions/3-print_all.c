c7d4ea4bbdd7e362c5c38515ced2a35a8af1786d
chandem <49794721+chandem@users.noreply.github.com>
2022-08-04 22:54:36 +0300

    Update 3-print_all.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,11 @@
+#include<stdarg.h>
+#include<stdio.h>
+#include"variadic_functions.h"
+/**
+*print_all -function that print s all 
+*@format: input parameter.
+*/
+
+void print_all(const char * const format, ...)
+{
+}
