ed0343cf2ddf90b32d673d8aaa2d51741e083c2c
chandem <49794721+chandem@users.noreply.github.com>
2022-08-04 21:18:23 +0300

    Update 1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,11 @@
+#include<stdarg.h>
+#include"variadic_function.h"
+/**
+*print_numbers -function that print numbers.
+*@separator: input parameter.
+*@n: input parameter.
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+
+}
