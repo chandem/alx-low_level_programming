f1c2c94d69963f6eb39735aa73a960e0e1ca0321
chandem <49794721+chandem@users.noreply.github.com>
2022-08-04 20:53:42 +0300

    Update 0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,10 @@
+#include"variadic_functions.h"
+/**
+*sum_them_all -function that sum for given numbers.
+*@n: input parameter.
+*Return: int
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+
+}
