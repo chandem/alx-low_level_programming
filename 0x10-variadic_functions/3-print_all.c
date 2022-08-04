a1395609e7524d552256c1ef4871be80c876e2f8
chandem <49794721+chandem@users.noreply.github.com>
2022-08-04 23:57:01 +0300

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,8 +9,6 @@
 void print_all(const char * const format, ...)
 {
 int i;
-char c;
-float d;
 char *h;
 va_list args;
     va_start(args, format);
@@ -22,16 +20,7 @@ va_list args;
              i = va_arg(args, int);
             printf("%d\n", i);
         } 
-else if (*format == 'c') 
-{
-             c = va_arg(args, char);
-            printf("%c\n", c);
-        } 
-else if (*format == 'f')
- {
-             d = va_arg(args,float);
-            printf("%f\n", d);
-        }
+
 else if (*format == 's')
 {
 h = va_arg(args, char *);
