fa60f506017aac9e833d1c5c8771b123ccfe3e1a
chandem <49794721+chandem@users.noreply.github.com>
2022-08-04 21:21:09 +0300

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,1 +1,9 @@
+#define VARIADIC_FUNCTION_
+#define VARIADIC_FUNCTION_
+
+#include<stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+
+#endif
