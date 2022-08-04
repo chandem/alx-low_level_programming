e802578c17829e419f2190837e5ae7600d0393df
chandem <49794721+chandem@users.noreply.github.com>
2022-08-04 20:59:10 +0300

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,5 @@
-#include"variadic_functions.h"
+#include <stdarg.h>
+#include"variadic_functions.h" 
 /**
 *sum_them_all -function that sum for given numbers.
 *@n: input parameter.
@@ -7,4 +8,19 @@
 int sum_them_all(const unsigned int n, ...)
 {
 
+
+    int Sum = 0;
+
+    va_list ptr;
+
+    va_start(ptr, n);
+
+    for (int i = 0; i < n; i++)
+
+        Sum += va_arg(ptr, int);
+
+    va_end(ptr);
+ 
+
+    return sum;
 }
