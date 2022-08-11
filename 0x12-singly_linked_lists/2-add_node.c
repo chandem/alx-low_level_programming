d3a93fc7b9765d99ae829f069ebd61f7e5e8594a
chandem <49794721+chandem@users.noreply.github.com>
2022-08-11 21:26:32 +0300

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,15 +11,15 @@ list_t *newnode
 size_t d;
 
     newnode =(list_t)malloc(sizeof(list_t));
+    newnode->str = strdup(str);
+    for (d = 0; str[d]; d++);
 
     if(newnode == NULL)
     {
         return NULL;
     }
 
-	newnode->str = strdup(str);
 
-	for (d = 0; str[d]; d++)
     else
     {
         newnode->len = len;
