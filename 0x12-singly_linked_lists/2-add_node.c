08580ceb1c7b7bfa2af6f3736321549a8561585c
chandem <49794721+chandem@users.noreply.github.com>
2022-08-11 20:56:52 +0300

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,1 +1,11 @@
+#include"lists.h"
+/**
+*add_node -function that add node at beginning.
+*@head: input parameter.
+*@str: input parameter.
+*Return: list_t.
+*/
+list_t *add_node(list_t **head, const char *str)
+{
 
+}
