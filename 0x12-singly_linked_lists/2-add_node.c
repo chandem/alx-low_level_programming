2d554e0da4b05f207261229058263598e84543d7
chandem <49794721+chandem@users.noreply.github.com>
2022-08-11 21:17:58 +0300

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,8 +7,9 @@
 */
 list_t *add_node(list_t **head, const char *str)
 {
+list_t *newnode
 
-    newNode = (struct node*)malloc(sizeof(struct node));
+    newNode = (list_t)malloc(sizeof(list_t));
 
     if(newNode == NULL)
     {
