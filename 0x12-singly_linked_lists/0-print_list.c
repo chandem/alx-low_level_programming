ec0e9210bd1a68bbfac6d7e95cd86c6b51e8e15b
chandem <49794721+chandem@users.noreply.github.com>
2022-08-11 19:31:01 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,4 @@
-#include"lists.h"
+include"lists.h"
 /**
 *print_list -function that print list.
 *@h: input parameter.
@@ -6,9 +6,19 @@
 */
 size_t print_list(const list_t *h)
 {
-while (h != NULL) 
+typedef struct list_s
 {
-    printf("%d ", h->value);
-    h = h->next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+list_t *head=h;
+head->Len=0;
+while (head != NULL) 
+{
+    printf("%s", h->str);
+    head = head->next;
+head->Len++;
   }
+return (head->len);
 }
