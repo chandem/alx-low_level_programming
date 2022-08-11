04d4e2a19b3d1d1492e077aa9a44a31fe01d0cfd
chandem <49794721+chandem@users.noreply.github.com>
2022-08-11 20:34:12 +0300

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,1 +1,16 @@
-
+#include"lists.h"
+/**
+*print_list -function that print list.
+*@h: input parameter.
+*Return: size_t.
+*/
+size_t print_list(const list_t *h)
+{
+size_t r;
+r=0;
+while (h != NULL) 
+{
+r++;
+}
+return r;
+}
