4118a18f150fb56656148e2dd876686512c366a6
chandem <49794721+chandem@users.noreply.github.com>
2022-08-11 19:51:57 +0300

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,13 @@
 #ifndef _LISTS_
 #define _LISTS_
 
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
 size_t print_list(const list_t *h);
 
 #endif
