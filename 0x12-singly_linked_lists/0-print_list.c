f6c64e4f2fc9fe13b70a6b24f5e1fe97584fc78d
chandem <49794721+chandem@users.noreply.github.com>
2022-08-11 19:51:34 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,12 +10,6 @@
 */
 size_t print_list(const list_t *h)
 {
-typedef struct list_s
-{
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
 list_t *head=h;
 head->Len=0;
 if(head->str==NULL)
